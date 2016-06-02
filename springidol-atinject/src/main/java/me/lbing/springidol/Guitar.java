abac1972bc40f073cd71aed8a5081907bae5c164
JeremyKing <jgroups@163.com>
2016-06-02 08:55:40 +0800

    spring idol-@inject

---
+++ springidol-atinject/src/main/java/me/lbing/springidol/Guitar.java
@@ -0,0 +1,10 @@
+package me.lbing.springidol;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Guitar implements Instrument {
+  public void play() {
+    System.out.println("Strum strum strum");
+  }
+}
---
+++ springidol-atinject/src/main/java/me/lbing/springidol/Saxophone.java
@@ -0,0 +1,14 @@
+// <start id="piano_java" />
+package me.lbing.springidol;
+
+
+//@Component
+public class Saxophone implements Instrument {
+  public Saxophone() {
+  }
+
+  public void play() {
+    System.out.println("PLINK PLINK PLINK");
+  }
+}
+// <end id="piano_java" />
